351038bd3d62e0f17e559fbfcc7103cb60c43e69
Damir Garifullin <gosugdr@gmail.com>
2016-10-18 00:34:57 +0300

    Fixed grub

--- install_scratch.sh
+++ install_scratch.sh
@@ -49,4 +49,4 @@ arch-chroot /mnt mkinitcpio -p linux
 
 arch-chroot /mnt pacman -Sy grub efibootmgr --noconfirm
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
-arch-chroot /mnt grub-mkconfig -o /boot/grub
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
