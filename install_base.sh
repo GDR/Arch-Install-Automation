e252c9040232fb7a0c27533e0c71e2475119a180
Damir Garifullin <gosugdr@gmail.com>
2016-10-18 14:18:07 +0300

    Added post install script

--- install_base.sh
+++ install_base.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+# Install aur
+
 echo "[archlinuxfr]
 SigLevel = Never
 Server = http://repo.archlinux.fr/\$arch"
 pacman --noconfirm -Sy yaourt
+
+# Add GDR user and set password
+
+# Install git
+
+# Install awesome wm
+
+# Install google-chrome
+
+# Install docker
+
+# Install android-studio
--- install_scratch.sh
+++ install_scratch.sh
@@ -50,3 +50,7 @@ arch-chroot /mnt mkinitcpio -p linux
 arch-chroot /mnt pacman -Sy grub efibootmgr --noconfirm
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+# Copy this to new OS
+cp $(pwd) /mnt/root
+reboot
